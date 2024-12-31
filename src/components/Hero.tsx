579f5c42a616011f66e0dc8d29ec640d95abcda2
Nizar Fazari <nexsel32@gmail.com>
2024-12-31 14:19:30 +0700

    fixing navbar and not using effect in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,44 +1,21 @@
 import { motion } from "motion/react"
 
 interface HeroProps {
-    isVisible: boolean
     toggleFade : () => void
-
-
 }
 
 export function Hero({
-    isVisible,toggleFade
+    toggleFade
 }: HeroProps) {
-    console.log(isVisible)
     return (
         <section className="container mx-auto  flex flex-col md:flex-row items-center justify-between relative h-[300px]">
-            <div onClick={toggleFade} className="absolute top-0 left-0 w-full h-full z-20 cursor-pointer"></div>
-            <motion.div
-                initial={{ opacity: 1, y: 100 }}
-                animate={{ opacity: isVisible ? 0 : 1, y: isVisible ? 0 : 100 }}
-                transition={{ duration: 0.5 }}
-                className={`absolute top-0 left-0 w-full h-full flex items-center justify-center `}
-            >
-                <div className="relative flex items-center justify-center ">
-                  
-                    <motion.img
-                        initial={{ opacity: 1, y: 100 }}
-                        animate={{ opacity: isVisible ? 0 : 1, y: isVisible ? 0 : 100 }}
-                        transition={{ duration: 0.5 }}
-                        src="/home/arrow-down.png"
-                        alt="Arrow Down"
-                        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-[150px]"
-                    />
-
-                </div>
-            </motion.div>
+            <div onClick={toggleFade} className="absolute top-0 left-0 w-full h-full z-20 "></div>
+          
 
             <motion.div className="md:w-1/2 space-y-4"
                 initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : 100 }}
+                animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
-
             >
                 <h1 className="text-5xl md:text-6xl font-semibold leading-[80px] font-redhat pl-20">
                     Enhancing 
@@ -50,7 +27,7 @@ export function Hero({
             <div className="md:w-1/2 mt-8 md:mt-0 relative">
                 <motion.div
                     initial={{ y: 100, scale: 2 }}
-                    animate={{ x: isVisible ? 100 : -320, rotate: isVisible ? 10 : 0, }}
+                    animate={{ x: 100 , rotate:10, }}
                     transition={{ duration: 0.5 }}
                     className=""
                 >
