fc032abab0b7c6c8f62364850dd39811ce6e259a
Nizar Fazari <nexsel32@gmail.com>
2025-01-07 23:36:22 +0700

    added gap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@
 
 export function Hero() {
     return (
-        <section className="container mx-auto flex flex-col md:flex-row items-center justify-between relative h-auto md:h-[200px] px-6 md:px-20 py-12 md:py-0">
+        <section className="container mx-auto flex flex-col md:flex-row items-center justify-between relative h-auto md:h-[200px] gap-10 pt-10">
 
             <div className="w-full md:w-1/3 space-y-4"
             >
@@ -16,7 +16,7 @@ export function Hero() {
          
 
             <div className="w-full md:w-2/3 relative">
-                <div className="relative w-full h-[497px] w-[800px] overflow-hidden">
+                <div className="relative w-full h-[497px] max-w-[800px] overflow-hidden">
                     <video  controls className="absolute top-[-15px] px-4 left-0 w-full h-full rounded-md z-20">
                         <source src="/video/home.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
