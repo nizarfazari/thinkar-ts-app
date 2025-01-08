9675e142f6692b34ff0887a11a94b9173d23c430
Nizar Fazari <nexsel32@gmail.com>
2025-01-08 21:11:29 +0700

    fixing tablet and delete image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,16 +9,11 @@ export default function Home() {
 
     return (
         <main >
-
             <section className="min-h-[700px]  text-[#5F5F5F] overflow-hidden flex flex-col">
                 <div className="flex-1 pt-52">
                     <Hero />
                 </div>
-
-               
             </section>
-
-
             <section className="min-h-screen   pb-14 overflow-hidden relative">
                 <Carousel />
             </section>
@@ -30,8 +25,6 @@ export default function Home() {
             <section className="min-h-screen    overflow-x-clip">
                 <SubscriptionSection />
             </section>
-
-           
 
         </main>
     );
