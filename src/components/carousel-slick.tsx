9da44501acc931f47ed22716f1c2ff1d48ec695f
Nizar Fazari <nexsel32@gmail.com>
2025-01-08 23:15:00 +0700

    fixing image

--- src/components/carousel-slick.tsx
+++ src/components/carousel-slick.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import  { useState, useRef, useCallback } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import { ArrowRight } from 'lucide-react'
-import { Play } from '@phosphor-icons/react'
+import { Play, Pause } from '@phosphor-icons/react'
 import Slider from 'react-slick'
 import ReactPlayer from 'react-player'
 import { Card } from '@/components/ui/card'
@@ -65,11 +65,19 @@ export default function CarouselSlick() {
 
     const goToSlide = useCallback((index: number) => {
         setCurrentSlide(index)
+        setVideoVisible(index)
         sliderRef.current?.slickGoTo(index)
     }, [])
 
-
-
+    const toggleVideo = useCallback((index: number) => {
+        if (playingSlide === index) {
+            setPlayingSlide(null)
+        } else {
+            setPlayingSlide(index)
+            setVideoVisible(index)
+            goToSlide(index)
+        }
+    }, [playingSlide, goToSlide])
 
     const nextSlide = useCallback(() => {
         sliderRef.current?.slickNext()
@@ -87,8 +95,7 @@ export default function CarouselSlick() {
         focusOnSelect: true,
         beforeChange: (_: number, next: number) => {
             setCurrentSlide(next)
-            setPlayingSlide(null)
-            setVideoVisible(null)
+        
         },
         responsive: [
             {
@@ -107,7 +114,7 @@ export default function CarouselSlick() {
     }
 
     return (
-        <div >
+        <div>
             <div className="container mx-auto py-12">
                 {/* Header */}
                 <div className='flex flex-col md:flex-row mb-12 justify-between items-start md:items-center'>
@@ -118,8 +125,6 @@ export default function CarouselSlick() {
                         </h1>
                         <h2 className="text-4xl md:text-5xl leading-tight md:leading-[60px] font-semibold text-white">Global Impact.</h2>
                     </div>
-
-
                 </div>
 
                 {/* Carousel Container */}
@@ -130,20 +135,21 @@ export default function CarouselSlick() {
                                 <Card
                                     key={slide.id}
                                     onClick={() => goToSlide(index)}
-                                    className={`transform transition-all duration-500 !border-none ease-in-out group ${index === currentSlide
-                                        ? 'flex-grow-[2] opacity-100 w-full md:w-[600px]'
-                                        : 'flex-grow-0 opacity-60 w-[100px] md:w-[200px] cursor-pointer'
-                                        } relative rounded-3xl overflow-hidden`}
+                                    className={`transform transition-all duration-500 !border-none ease-in-out group ${
+                                        index === currentSlide
+                                            ? 'flex-grow-[2] opacity-100 w-full md:w-[600px]'
+                                            : 'flex-grow-0 opacity-60 w-[100px] md:w-[200px] cursor-pointer'
+                                    } relative rounded-3xl overflow-hidden`}
                                 >
                                     <div className="relative h-[300px] wrapper-vidio md:h-[400px]">
                                         {/* Thumbnail */}
                                         <img
                                             src={slide.thumbnailUrl}
                                             alt={`${slide.title} thumbnail`}
-                                            className={`absolute pointer-events-none inset-0 w-full h-full object-cover object-center transition-opacity duration-300 ${videoVisible === index ? 'opacity-0' : 'opacity-100'
-                                                }`}
+                                            className={`absolute pointer-events-none inset-0 w-full h-full object-cover object-center transition-opacity duration-300 ${
+                                                videoVisible === index ? 'opacity-0' : 'opacity-100'
+                                            }`}
                                         />
-
 
                                         {/* Video Player */}
                                         <ReactPlayer
@@ -154,30 +160,39 @@ export default function CarouselSlick() {
                                             height="100%"
                                             playing={playingSlide === index}
                                             loop
-                                            volume={0.5}
-                                            controls={index === currentSlide}
+                                            muted
                                             playsinline
                                             style={{ display: videoVisible === index ? 'block' : 'none' }}
                                             config={{
                                                 file: {
                                                     attributes: {
-                                                        style: { objectFit: 'cover' }
+                                                        style: { objectFit: 'cover' },
+                                                        playsInline: true,
+                                                        preload: 'auto',
                                                     }
                                                 }
                                             }}
+                                            onTouchEnd={(e : Event) => {
+                                                e.preventDefault()
+                                                toggleVideo(index)
+                                            }}
                                         />
-
 
                                         <button
                                             onClick={(e) => {
                                                 e.stopPropagation()
-                                                goToSlide(index)
+                                                toggleVideo(index)
                                             }}
-                                            className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  rounded-full p-4 transition-opacity duration-300 border-4 border-solid border-white ${videoVisible === index && playingSlide === index ? 'opacity-0' : 'opacity-100'
-                                                }`}
+                                            className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 transition-opacity duration-300 border-4 border-solid border-white ${
+                                                videoVisible === index && playingSlide === index ? 'opacity-0' : 'opacity-100'
+                                            }`}
                                             aria-label={playingSlide === index ? "Pause" : "Play"}
                                         >
-                                            <Play weight="fill" className="w-8 h-8 text-white" />
+                                            {playingSlide === index ? (
+                                                <Pause weight="fill" className="w-8 h-8 text-white" />
+                                            ) : (
+                                                <Play weight="fill" className="w-8 h-8 text-white" />
+                                            )}
                                         </button>
                                     </div>
                                 </Card>
@@ -202,12 +217,12 @@ export default function CarouselSlick() {
                                 onClick={() => goToSlide(index)}
                                 variant={currentSlide === index ? "default" : "ghost"}
                                 className={`
-                                                     px-3 py-1 md:px-6 md:py-2 text-sm md:text-base rounded-full transition-all shadow-lg
-                                                     ${currentSlide === index
+                                    px-3 py-1 md:px-6 md:py-2 text-sm md:text-base rounded-full transition-all shadow-lg
+                                    ${currentSlide === index
                                         ? 'bg-[#2697FF] text-white hover:bg-[#2697FF]/90'
                                         : 'bg-[#71AED0] text-white hover:text-white hover:bg-[#71AED0]/80'
                                     }
-                                                 `}
+                                `}
                             >
                                 {slide.title}
                             </Button>
