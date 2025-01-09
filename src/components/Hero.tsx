3b1aa1c32dc2f4a156b858b2f07389aff34ae194
Nizar Fazari <nexsel32@gmail.com>
2025-01-09 13:58:46 +0700

    added video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -58,16 +58,16 @@ export function Hero() {
 
     return (
         <section className="container mx-auto flex flex-col md:flex-row items-center justify-between relative h-auto md:h-[200px] gap-10 pt-10">
-            <div className="w-full md:w-1/2 space-y-4">
+            <div className="w-[800px]  space-y-4">
                 <h1 className="text-[2.5rem] sm:text-2xl md:text-6xl font-semibold leading-[1.3] sm:leading-[1.2] md:leading-[60px] font-redhat text-center md:text-left">
                     Enhancing <span className="text-blue-400">Vision</span> <br /> Empowering You
                 </h1>
             </div>
 
-            <div className="w-full md:w-2/3 relative shadow-wrapper">
-                <div className="relative max-w-[800px] mx-auto h-[497px] overflow-hidden">
+            <div className="w-[80%]  relative shadow-wrapper">
+                <div className="relative w-full mx-auto h-[600px] overflow-hidden">
                     <div 
-                        className="relative h-[480px] w-full cursor-pointer"
+                        className="relative h-[600px] w-full cursor-pointer"
                         onClick={toggleVideo}
                         onTouchEnd={toggleVideo}
                         role="button"
